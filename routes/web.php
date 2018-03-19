628cb4f19ee6a02d70c8156bb1a8d2943b6e6033
Ashish <ashish.dewangan@mindzglobal.com>
2018-03-19 16:00:37 +0530

    ghjh

--- routes/web.php
+++ routes/web.php
@@ -19,22 +19,6 @@ Route::get('/', 'HomeController@about')->name('home');
 //College Section  ##############################################################################
 Route::get('college/login','College\CollegeAuthController@login');
 Route::get('college/register','College\CollegeAuthController@register');
-<<<<<<< HEAD
-=======
-Route::get('college/reg','College\CollegeAuthController@register');
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> 41af0a053a6c787a2b1b7634fbd0bea7db37b878
 
 Route::get('college/demo', function () {
     return view('college.create_profile');
