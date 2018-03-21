1da8318ad403d92c1cf516f498405a94ea52b595
Selvan <thamaraiselvan689@gmail.com>
2018-03-21 13:48:31 +0530

    database related to student

---
+++ app/students/Student_Education_Details.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\students;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student_Education_Details extends Model
+{
+    //
+}
---
+++ app/students/Student_Graduation__Details.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\students;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student_Graduation__Details extends Model
+{
+    //
+}
