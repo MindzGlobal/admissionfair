9364b71fa61bd89deed41c277e7637dd169edb89
Roopa <roopa.lucky30@gmail.com>
2018-04-07 12:58:29 +0530

    Couse Details

--- resources/views/student/pages/course_department.blade.php
+++ resources/views/student/pages/course_department.blade.php
@@ -3,7 +3,27 @@
 
 @section('css')
 <style type="text/css">
-
+hr {
+    margin-top: 20px;
+    margin-bottom: 0px!important;
+    border: 0;
+    border-top: 1px solid #eee;
+}
+.teacher-detail p {
+    color: #3f3f3f;
+    font-weight: 600;
+    margin: 4px 0 0px;
+    text-align: left;
+    padding-top: 0px;
+}
+span.clg-deiatls {
+	float: right;
+	color: #33333391;
+}
+h4
+{
+	margin-top:0px!important;
+}
 </style>
 @endsection
 
@@ -81,7 +101,28 @@
                               {{$course->course_description}}<i class="fa fa-quote-right"></i>
                            </blockquote>
                         </div>
-                        <div class="course-info col-md-9">
+						<div class="row">
+						<div class="course-teacher col-md-6">
+                                <h3>College Details</h3>
+
+                                <div class="teacher-detail bg-gray">
+                                   <p>University Name: <span class="clg-deiatls">   {{$college->university_name}}</span></p>
+									<hr class="list-inline">
+									<p>Email: <span class="clg-deiatls"> {{$college->college_email}}</span> </p>
+									<hr class="list-inline">
+									<p>Contact no: <span class="clg-deiatls">  {{$college->college_number_1}},{{$college->college_number_2}}</span> </p>
+									<hr class="list-inline">
+									<p>College Type:  <span class="clg-deiatls">  {{$college->college_type}}</span> </p>
+									<hr class="list-inline">
+									<p>College Category:  <span class="clg-deiatls"> {{$college->college_category}}</span> </p>
+									<hr class="list-inline">
+									<p>College Address:  <span class="clg-deiatls"> {{$college->college_address}},{{$college->college_state}}</span> </p>
+									<hr class="list-inline">
+                                 
+
+                                  </div>
+                            </div>
+                        <div class="course-info col-md-6">
                            <h3 class="sidebar-title">Courses Information</h3>
                            <ul class="bg-gray"> 
                               <li>Course :<span>{{$course->course_offer}}</span></li>
@@ -98,6 +139,8 @@
                         </ul>
                            <button class="btn more-link pull-right next-step" id="send" data-toggle="modal" data-target="#apply">Apply</button>
                         </div>
+						
+							</div>
                         <br>
                         </br>
                      </article>
@@ -108,42 +151,28 @@
                <div class="col-sm-12 col-md-4 sidber">
                   <!-- // End Widget -->
                   <div class="widget">
-                     <div class="wiget-title">
-                        <h4>Populer Colleges</h4>
+				  	  <div class="wiget-title">
+                      
+                        <h4>Contact Person</h4>
+                    </div>
+                    <div class="teacher-detail bg-gray">
+				
+                                      @if(!is_null($college->profile_image))
+                              <img class="img-responsive" src="{{ asset($college->profile_image) }}" alt="Blog_image">
+                             @else
+                             <img class="img-responsive" src="{{ asset('student/images/blog/1.jpg') }}" alt="Blog_image">
+                              @endif
+							  <br>
+                                    <h4>{{$college->name}}</h4>
+
+                                    <p>Email: <span class="clg-deiatls">  {{$college->email}}</span></p>
+									<hr class="list-inline">
+									<p>Contact No: <span class="clg-deiatls">  {{$college->mobile}}</span></p>
+									<hr class="list-inline">
+									
+                                </div>
                      </div>
-                     <div class="sidber-widget">
-                        <div class="wiget-post">
-                           <div class="wiget-img">
-                              <img src="{{ asset('student/images/blog/1.jpg')}}" alt="Sidber Image">
-                           </div>
-                           <div class="posi-inner">
-                              <h6><a href="#">Students Have Enough</a></h6>
-                              <span>John Milton</span>
-                              <p>Price: <strong>Free</strong></p>
-                           </div>
-                        </div>
-                        <div class="wiget-post">
-                           <div class="wiget-img">
-                              <img src="{{ asset('student/images/blog/1.jpg')}}" alt="Sidber Image">
-                           </div>
-                           <div class="posi-inner">
-                              <h6><a href="#">Students Have Enough</a></h6>
-                              <span>John Milton</span>
-                              <p>Price: <strong>$29</strong></p>
-                           </div>
-                        </div>
-                        <div class="wiget-post">
-                           <div class="wiget-img">
-                              <img src="{{ asset('student/images/blog/1.jpg') }}" alt="Sidber Image">
-                           </div>
-                           <div class="posi-inner">
-                              <h6><a href="#">Students Have Enough</a></h6>
-                              <span>John Milton</span>
-                              <p>Price: <strong>$50</strong></p>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
+                 
                   <!-- // End Widget -->
                   <div class="widget">
                      <div class="video-widget clearfix">
