7fbee6970e21075858f40bc16e59b8debcc11456
Mala <malamaps.26@gmail.com>
2018-04-07 13:00:30 +0530

    gallery

---
+++ resources/views/student/pages/video_gallery.blade.php
@@ -0,0 +1,74 @@
+@extends('student.layouts.student_general')
+@section('title', "Gallery")
+@section('css')
+
+@endsection
+
+@section('content')
+      <!-- Strat Banner Section -->
+      <div class="edu-banner padding-bt-150">
+                  <div class="container">
+                     <div class="count-particles">
+                        <span class="js-count-particles"></span>
+                     </div>
+                     <div class="row">
+                        <div class="col-sm-12">
+                           <div class="page-title-box">
+                              <h2>Video Gallery</h2>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- particles.js container -->
+               </div>
+               <!-- End Banner Section -->
+               <!-- Start form section-->
+               <!-- Start Portfolio Section -->
+               <section class="gallery-area">
+                  <div class="container">
+                     <div class="row clearfix" style="box-shadow:1px 0px 15px;padding:10px;">
+                        <div class="gallery-filter-item text-center">
+                           <h3><u>Video Gallery</u></h3>
+                        </div>
+                        <div class="gallery col-3 gutter">
+                           <div class="gallery-item gp-two">
+                              <div class="thumb">
+                                 <iframe class="video-size" src="https://www.youtube.com/embed/KNChk5o8Fhg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                           </div>
+                           <div class="gallery-item gp-three">
+                              <div class="thumb">
+                                 <iframe class="video-size" src="https://www.youtube.com/embed/KNChk5o8Fhg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                           </div>
+                           <div class="gallery-item gp-four">
+                              <div class="thumb">
+                                 <iframe class="video-size" src="https://www.youtube.com/embed/KNChk5o8Fhg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                           </div>
+                           <div class="gallery-item gp-five">
+                              <div class="thumb">
+                                 <iframe class="video-size" src="https://www.youtube.com/embed/KNChk5o8Fhg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                           </div>
+                           <div class="gallery-item gp-five">
+                              <div class="thumb">
+                                 <iframe class="video-size" src="https://www.youtube.com/embed/KNChk5o8Fhg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                           </div>
+                           <div class="gallery-item gp-two">
+                              <div class="thumb">
+                                 <iframe class="video-size" src="https://www.youtube.com/embed/KNChk5o8Fhg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </section>
+      
+      @endsection
+
+      @section('js')
+      
+
+      @endsection
