b009356820925a192b158a3fda3a00349bf15bac
Ashish <ashish.dewangan@mindzglobal.com>
2018-04-10 15:05:11 +0530

    addition

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,10 +11,10 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('g');
+    // }
 
     /**
      * Show the application dashboard.
@@ -23,11 +23,7 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        session(['name' => 'Ashish']);
-        $value = $request->session()->get('name');
-        //dd(session()->all());
-        $ses = session()->all();
-        return view('home')->with(['data'=>$value]);
+        return view('index');
     }
 
     public function about()
